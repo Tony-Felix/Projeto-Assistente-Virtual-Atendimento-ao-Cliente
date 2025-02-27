e9444079830f4aa0a0b39f09759262b736ffa704
tony <toni.felixx@gmail.com>
2025-02-27 10:57:21 -0300

    roda flask e gradio ao mesmo tempo usando threading. comenta codio do iflame

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
+import threading
 from flask import Flask, redirect, render_template  # type: ignore
+import gradio as gr  # type: ignore
+from gemini_config import gradio_wrapper
 
 app = Flask(__name__)
 
@@ -9,11 +12,27 @@ def index():
     return render_template("index.html")
 
 
+# Crie e lance a interface do chat com suporte a arquivos
+chat_interface = gr.ChatInterface(
+    fn=gradio_wrapper,
+    title="Chatbot! 💬 Assistente Virtual 💬",
+    multimodal=True
+)
+
+
 # Rota do chat
 @app.route("/chat")
 def chat_render():
     return redirect("http://127.0.0.1:7860")
 
 
+def run_flask():
+    app.run(host="127.0.0.1", port=5000, debug=False)
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    # app.run(debug=True)
+    threading.Thread(target=run_flask, daemon=True).start()
+    chat_interface.launch(
+        server_name="127.0.0.1", server_port=7860
+    )
--- gradio.app.py
+++ gradio.app.py
@@ -1,11 +1,11 @@
-import gradio as gr  # type: ignore
-from gemini_config import gradio_wrapper
+# import gradio as gr  # type: ignore
+# from gemini_config import gradio_wrapper
 
-# Crie e lance a interface do chat com suporte a arquivos
-chat_interface = gr.ChatInterface(
-    fn=gradio_wrapper,
-    title="Chatbot! 💬 Assistente Virtual 💬",
-    multimodal=True
-)
-# Inicie a interface
-chat_interface.launch(server_name="127.0.0.1", server_port=7860)
+# # Crie e lance a interface do chat com suporte a arquivos
+# chat_interface = gr.ChatInterface(
+#     fn=gradio_wrapper,
+#     title="Chatbot! 💬 Assistente Virtual 💬",
+#     multimodal=True
+# )
+# # Inicie a interface
+# chat_interface.launch(server_name="127.0.0.1", server_port=7860)
